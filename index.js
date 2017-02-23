0182d7391d413f4daa7b60f095970098aa58d055
Goyza II <goyza@Goyza.local>
2017-02-23 17:47:32 +0100

    Desktop version

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-
-var express = require('express');
 var app = require('express')();
-
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
-app.use(express.static('public'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -15,17 +10,14 @@ io.on('connection', function(socket){
   
   socket.on('position', function(datas){
     io.emit('position', datas);
-    console.log('position', datas);
   });
   
   socket.on('create', function(datas){
     io.emit('create', datas);
-    console.log('create', datas);
   });
   
   socket.on('collision', function(datas){
     io.emit('collision', datas);
-    console.log('collision', datas);
   });
   
 });
