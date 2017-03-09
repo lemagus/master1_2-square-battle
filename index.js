414cbdce8cab6d9d1b9299715d8c62fb2edbefc8
Goyza II <goyza@Goyza.local>
2017-03-09 17:56:02 +0100

    Server log

--- index.js
+++ index.js
@@ -2,22 +2,38 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var players = [];
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  
+    
+  socket.emit('players', players);
+   
   socket.on('position', function(datas){
     io.emit('position', datas);
   });
   
   socket.on('create', function(datas){
     io.emit('create', datas);
+    players.push(datas);
   });
   
   socket.on('collision', function(datas){
     io.emit('collision', datas);
+  });
+  
+  socket.on('leave', function(datas){
+	  var uid = datas.uniqid;
+	  for(var p in players) {
+		  var player = players[p];
+		  if( player.indexOf(uid) > -1 ){
+			  players.splice(p, 1);
+			  io.emit('delete', player);
+		  }
+	  }
   });
   
 });
